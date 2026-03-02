881cefce5dcefb5a6e7b13df703fd200fc99ce17
subframe7536 <1667077010@qq.com>
2026-03-03 00:08:47 +0800

    optimize card

--- src/card/card.tsx
+++ src/card/card.tsx
@@ -1,58 +1,87 @@
-import type { JSX } from 'solid-js'
+import type { JSX, ParentProps } from 'solid-js'
 import { Show } from 'solid-js'
 
 import type { SlotClasses } from '../shared/slot-class'
 import { cn } from '../shared/utils'
 
-type CardSlots = 'root' | 'header' | 'body' | 'footer'
+type CardSlots = 'root' | 'header' | 'title' | 'description' | 'action' | 'body' | 'footer'
 
 export type CardClasses = SlotClasses<CardSlots>
 
 export interface CardBaseProps {
+  compact?: boolean
+  title?: JSX.Element
+  description?: JSX.Element
   header?: JSX.Element
   footer?: JSX.Element
+  action?: JSX.Element
   classes?: CardClasses
-  children?: JSX.Element
 }
 
-export type CardProps = CardBaseProps
+export type CardProps = ParentProps<CardBaseProps>
 
 export function Card(props: CardProps): JSX.Element {
   return (
     <div
       data-slot="root"
       class={cn(
-        'ring-foreground/10 bg-card text-card-foreground gap-4 overflow-hidden divide-y divide-border rounded-xl py-4 text-sm ring-1 *:[img:first-child]:rounded-t-xl *:[img:last-child]:rounded-b-xl flex flex-col',
-        props.footer && 'pb-0',
+        'relative flex flex-col rounded-2xl border bg-card not-dark:bg-clip-padding text-card-foreground shadow-xs/5',
         props.classes?.root,
       )}
     >
-      <Show when={props.header}>
+      <Show when={props.header || props.title || props.description}>
         <div
           data-slot="header"
           class={cn(
-            'gap-1 rounded-t-xl px-4 is-[.border-b]:pb-4 grid auto-rows-min items-start',
+            'grid auto-rows-min items-start',
+            !props.header && (props.compact ? 'p-4 gap-1' : 'p-6 gap-2'),
+            props.action && 'grid-cols-[1fr_auto]',
             props.classes?.header,
           )}
         >
-          {props.header}
+          <Show when={props.title || props.description} fallback={props.header}>
+            <Show when={props.title}>
+              <div
+                data-slot="title"
+                class={cn('font-semibold text-lg leading-none', props.classes?.title)}
+              >
+                {props.title}
+              </div>
+            </Show>
+            <Show when={props.description}>
+              <p
+                data-slot="description"
+                class={cn('text-sm text-muted-foreground', props.classes?.description)}
+              >
+                {props.description}
+              </p>
+            </Show>
+            <Show when={props.action}>
+              <div
+                data-slot="action"
+                class={cn(
+                  'col-start-2 row-span-2 row-start-1 self-start justify-self-end inline-flex',
+                  props.classes?.action,
+                )}
+              >
+                {props.action}
+              </div>
+            </Show>
+          </Show>
         </div>
       </Show>
 
       <Show when={props.children}>
-        <div data-slot="body" class={cn('flex-1 px-4', props.classes?.body)}>
+        <div
+          data-slot="body"
+          class={cn('flex-1', props.compact ? 'px-4' : 'px-6', props.classes?.body)}
+        >
           {props.children}
         </div>
       </Show>
 
       <Show when={props.footer}>
-        <div
-          data-slot="footer"
-          class={cn(
-            'flex items-center bg-muted/50 rounded-b-xl b-t-(1 border) p-4',
-            props.classes?.footer,
-          )}
-        >
+        <div data-slot="footer" class={cn(props.compact ? 'p-4' : 'p-6', props.classes?.footer)}>
           {props.footer}
         </div>
       </Show>
--- todo.md
+++ todo.md
@@ -4,7 +4,7 @@
 - [x] add Image, inline kobalte's image to reduce context and jsx props pass
 - [x] add Avatar based on Image, support group.
 - [x] checkbox group table clickable area should expand, border overlapped should handle
-- [ ] card style port from zaidan/ example and update demo
+- [x] card style port from zaidan/ example and update demo
 - [ ] unify classes.root, prefer to use rest props to simplify splitProps groups
 - [ ] migrate custom render usage from function to jsx for fine grain update
 
