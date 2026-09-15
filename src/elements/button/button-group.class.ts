0f684f7ca532bb7bb265927c66130e5a47a96157
subframe7536 <1667077010@qq.com>
2026-09-15 11:21:31 +0800

    remove gap by default, todo: bring separator back

--- src/elements/button/button-group.class.ts
+++ src/elements/button/button-group.class.ts
@@ -7,7 +7,7 @@ export const buttonGroupRecipe = /* @__PURE__ */ slotRecipe<
   ButtonGroupT.Variant
 >({
   base: {
-    root: 'inline-flex w-fit items-stretch *:focus-visible:(relative z-sticky)',
+    root: 'inline-flex size-fit items-stretch *:focus-visible:(relative z-sticky)',
   },
   defaults: {
     orientation: 'horizontal',
@@ -15,10 +15,10 @@ export const buttonGroupRecipe = /* @__PURE__ */ slotRecipe<
   variants: {
     orientation: {
       horizontal: {
-        root: 'flex-row -[&>*:not(:last-child)]:me-px [&>*:not(:first-child)]:rounded-s-none [&>*:not(:last-child)]:rounded-e-none',
+        root: 'flex-row [&>*:not(:last-child)]:(-me-px border-e-0) [&>*:not(:first-child)]:rounded-s-none [&>*:not(:last-child)]:rounded-e-none',
       },
       vertical: {
-        root: 'flex-col -[&>*:not(:last-child)]:mb-px [&>*:not(:first-child)]:rounded-t-none [&>*:not(:last-child)]:rounded-b-none',
+        root: 'flex-col [&>*:not(:last-child)]:(-mb-px border-b-0) [&>*:not(:first-child)]:rounded-t-none [&>*:not(:last-child)]:rounded-b-none',
       },
     },
   },
