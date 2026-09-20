5b49b43120fd504553b37a53a9907a1b750969de
Song <1667077010@qq.com>
2026-09-20 19:11:00 +0800

    fix(checkbox): bridge visual focus lifecycle

--- src/forms/checkbox/checkbox.tsx
+++ src/forms/checkbox/checkbox.tsx
@@ -284,6 +284,19 @@ export function Checkbox<TTrue = boolean, TFalse = boolean>(
     }
   }
 
+  function onControlFocus(event: FocusEvent): void {
+    if (merged.fieldBind !== false) {
+      field.emit('focus', event)
+    }
+  }
+
+  function onControlBlur(event: FocusEvent): void {
+    enterPressed = false
+    if (merged.fieldBind !== false) {
+      field.emit('blur', event)
+    }
+  }
+
   const onPointerDown: JSX.EventHandler<HTMLButtonElement, PointerEvent> = (event) => {
     const { defaultPrevented } = callHandler(event, merged.onPointerDown)
     if (defaultPrevented) {
@@ -373,9 +386,8 @@ export function Checkbox<TTrue = boolean, TFalse = boolean>(
           onClick={onControlClick}
           onKeyDown={onControlKeyDown}
           onKeyUp={onControlKeyUp}
-          onBlur={() => {
-            enterPressed = false
-          }}
+          onFocus={onControlFocus}
+          onBlur={onControlBlur}
           {...checkboxAriaAttrs()}
           data-checked={resolvedChecked() ? '' : undefined}
           data-disabled={field.disabled() ? '' : undefined}
